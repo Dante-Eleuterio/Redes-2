9fce7301828fa819cbe1992fe61db39b39ffa487
Dante-Eleuterio <danteeleuterio00@gmail.com>
2023-10-18 20:14:26 -0300

    almost done

--- c.py
+++ c.py
@@ -5,26 +5,54 @@ import struct
 BUFFER_SIZE = 65507
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 5007
-
-def receive_data(sock, buffer, last_counter):
+def receive_data(sock, buffer, last_counter,out_order_packets,lost_packets):
+    #Abre o VLC para streamar o vídeo
     player = subprocess.Popen(["vlc", "fd://0"], stdin=subprocess.PIPE)
+    timeout=0
     while True:
         data, _ = sock.recvfrom(BUFFER_SIZE)
         counter = int.from_bytes(data[:4], 'big')
+        #Caso seja o primeiro pacote recebido
         if last_counter == 0:
             last_counter = counter
             player.stdin.write(data[4:])
         else:
+            #Se é o pacote esperado
             if counter == last_counter + 1:
+                if len(buffer)>0:
+                    out_order_packets+=1
+                timeout=0
                 player.stdin.write(data[4:])
                 last_counter += 1
-
-                while last_counter + 1 in buffer:
-                    player.stdin.write(buffer[last_counter + 1])
-                    del buffer[last_counter + 1]
-                    last_counter += 1
+                #Percorre o buffer de pacotes fora de ordem os escrevendo e contabiliza o número de pacotes perdidos
+                while len(buffer)>0:
+                        if last_counter+1 in buffer:
+                            while last_counter + 1 in buffer:
+                                player.stdin.write(buffer[last_counter + 1])
+                                del buffer[last_counter + 1]
+                                last_counter += 1
+                        else:
+                            last_counter+=1
+                            lost_packets+=1
             else:
-                buffer[counter] = data[4:]
+                #Caso tenha estourado o número de tentativas de receber o pacote certo
+                if timeout==10:
+                    timeout=0
+                    lost_packets+=1
+                    #Percorre o buffer de pacotes fora de ordem os escrevendo e contabiliza o número de pacotes perdidos
+                    while len(buffer)>0:
+                        if last_counter+1 in buffer:
+                            while last_counter + 1 in buffer:
+                                player.stdin.write(buffer[last_counter + 1])
+                                del buffer[last_counter + 1]
+                                last_counter += 1
+                        else:
+                            last_counter+=1
+                            lost_packets+=1
+                else:
+                    timeout+=1
+                    out_order_packets+=1
+                    buffer[counter] = data[4:]
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -33,8 +61,10 @@ def main():
     mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
     last_counter = 0
+    out_order_packets=0
+    lost_packets=0
     buffer = {}
-    receive_data(sock,buffer,last_counter)
+    receive_data(sock,buffer,last_counter,out_order_packets,lost_packets)
     sock.close()
 if __name__ == '__main__':
     main()
--- s.py
+++ s.py
@@ -1,35 +1,77 @@
 import socket
 import time
-import pickle
-
-# Configuração do servidor
+import os
+import subprocess
+from moviepy.editor import VideoFileClip
+import re
+# Configuração
+#  do servidor
 VIDEO = "hls/1080_video16.ts"
-BUFFER_SIZE = 65507  # MTU IPV4 - 20 (IP header) - 8 (UDP header)
-MCAST_GRP = '224.1.1.1'
-MCAST_PORT = 5007
+DIR = '/hls'
+BUFFER_SIZE = 65507 
+MCAST_GRP = '224.1.1.1' #IP Multicast
+MCAST_PORT = 5007 #Porta Multicast
 MULTICAST_TTL = 2
 
+#Função para ordenar os arquivos ts
+def extract_number(filename):
+    parts = filename.split("video")
+    if len(parts) > 1:
+        number = int(parts[1].split(".")[0])
+        return number
+
+#Função para ler o vídeo
 def read_video(filename):
     with open(filename, 'rb') as file:
         while True:
-            data = file.read(BUFFER_SIZE - 4)  # 4 bytes reserved for the counter
+            data = file.read(BUFFER_SIZE - 4)  # 4 bytes reservados para o contador
             if not data:
                 file.close()
                 break
-            yield data
+            yield data 
+
+#Calcular o bitrate do video
+def get_video_bitrate(filename):
+    cmd = ["ffmpeg", "-i", filename]
+    result = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = result.communicate()
+
+    #Procura pelo bitrate no outpu do FFmpeg
+    matches = re.search(r"bitrate: (\d+) kb/s", stdout.decode('utf-8'))
+    if matches:
+        return int(matches.group(1)) * 1000  # Converte kbps to bps
+
+
+def get_bps(filename):
+    bitrate = get_video_bitrate(filename)
+    bytes_per_second = bitrate / 8
+    return bytes_per_second
 
 def main():
+    #Definição dos sockets multicast
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
     counter=0
-    input("batata")
+    os.chdir("./hls")
+    files =[]
+    bitrates= []
+    #Itera sobre o diretório resgatando os nomes dos arquivos .ts
+    for filename in os.listdir('.'):
+        if filename.endswith(".ts"):
+            files.append(filename)
+    #Coloca os arquivos .ts em ordem
+    files = sorted(files,key=extract_number)
+    #Calcula o bitrate de cada arquivo
+    for f in files:
+        bitrates.append(get_bps(f))
+    #Começa a transmissão
     while True:
-        print("começando")
-        for data in read_video(VIDEO):
-            counter += 1
-            msg = counter.to_bytes(4, 'big') + data  # Prefix the data with the counter
-            sock.sendto(msg, (MCAST_GRP, MCAST_PORT))
-            time.sleep(0.1)
-
+        for index,f in enumerate(files):
+            for data in read_video(f):
+                counter += 1
+                msg = counter.to_bytes(4, 'big') + data  # Prefix the data with the counter
+                sock.sendto(msg, (MCAST_GRP, MCAST_PORT))
+                sleep_duration = len(data) / bitrates[index]
+                time.sleep(sleep_duration)
 if __name__ == '__main__':
     main()
